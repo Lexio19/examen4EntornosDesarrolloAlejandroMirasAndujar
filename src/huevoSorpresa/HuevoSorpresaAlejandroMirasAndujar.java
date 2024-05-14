b7e6a55912b104a11f112efadb14fc83459b483c
Alejandro Miras <alejandromirasandujar@gmail.com>
2024-05-14 12:11:48 +0200

    Proyecto documentado

--- src/huevoSorpresa/HuevoSorpresaAlejandroMirasAndujar.java
+++ src/huevoSorpresa/HuevoSorpresaAlejandroMirasAndujar.java
@@ -1,14 +1,28 @@
 package huevoSorpresa;
+/** Este es el Javadoc de la clase HuevoSorpresa
+ * @author alex_
+ * @version 1.0
+ * @since 14 de mayo de 2024
+ */
 public class HuevoSorpresaAlejandroMirasAndujar {
     private int unidades;//número de huevos que quedan 
     private double precio;//precio actual de un huevo sorpresa
     private String sorpresa;//nombre de la sorpresa que incluye huevo sorpresa
     private double precio_max;//precio máximo que puede tener un huevo sorpresa
     
-    
+    /**
+     * Constructor sin parámetros, por defecto
+     */
 public HuevoSorpresaAlejandroMirasAndujar(){}
 
-
+/**
+ * Este es el constructor con parámetros. El objeto, para instanciarse, necesita
+ * recibir las unidades, el precio, la sorpresa que lleva dentro y el precio máximo
+ * @param unidades las unidades que hay del objeto HuevoSorpresa
+ * @param precio el precio del objeto HuevoSorpresa
+ * @param sorpresa la sorpresa que lleva dentro el objeto HuevoSorpresa
+ * @param precio_max el precio máximo del objeto HuevoSorpresa
+ */
 public HuevoSorpresaAlejandroMirasAndujar(int unidades, double precio, String sorpresa, double precio_max){
     this.unidades=unidades;
     this.precio=precio;
@@ -16,14 +30,33 @@ public HuevoSorpresaAlejandroMirasAndujar(int unidades, double precio, String so
     this.precio_max=precio_max;
 }
 
-
+/**
+ * Método que nos permite saber cuántas unidades hay de un objeto HuevoSorpresa
+ * @return devuelve las unidades que hay de un objeto HuevoSorpresa
+ */
 public int obtenerUnidades(){return this.getUnidades();}
 
-
+/**
+ * Método que nos permite saber el precio del objeto HuevoSorpresa
+ * @return devuelve el precio del objeto HuevoSorpresa
+ */
 public double obtenerPrecio(){return this.getPrecio();}
 
-
+/**
+ * Método que nos permite modificar las unidades de un objeto HuevoSorpresa
+ * @param unidades hay que introducir el nuevo número de unidades del objeto
+ * HuevoSorpresa
+ */
 public void modificarUnidades(int unidades){this.setUnidades(unidades);}
+
+/**
+ * Método que nos permite sacar huevos sorpresa 
+ * @param unidades el número de huevos que queremos sacar
+ * @param dinero el dinero que introducimos para sacar huevos
+ * @param sorpresa la sorpresa que lleva dentro
+ * @throws Exception salta la excepción si la cantidad de dinero es negativa, o no hay suficiente dinero o 
+ * indicamos una cantidad negativa de huevos sorpresa
+ */
 
 public void sacarHuevosSorpresas(int unidades, double dinero, String sorpresa) throws Exception{
    if (dinero <= 0) {
@@ -41,7 +74,11 @@ public void sacarHuevosSorpresas(int unidades, double dinero, String sorpresa) t
     this.modificarUnidades(this.obtenerUnidades()-unidades);
 }
 
-
+/**
+ * Método que nos permite aumentar el precio de los objetos HuevoSorpresa
+ * @param aumento escribimos la cantidad que aumentará el precio del objeto HuevoSorpresa
+ * @throws Exception salta la excepción si el aumento es negativo o superamos el precio máximo
+ */
 public void aumentarPrecio(double aumento) throws Exception{
     if(aumento <= 0) {
         throw new Exception("El aumento debe ser positivo");
@@ -52,57 +89,57 @@ public void aumentarPrecio(double aumento) throws Exception{
         setPrecio(getPrecio() + aumento);
 }
 
-    /**
-     * @return the unidades
+    /** Método get para saber las unidades del objeto HuevoSorpresa
+     * @return devuelve las unidades de objetos HuevoSorpresa
      */
     public int getUnidades() {
         return unidades;
     }
 
-    /**
-     * @param unidades the unidades to set
+    /** Método que permite modificar las unidades del objeto HuevoSorpresa
+     * @param unidades introducimos el nuevo número de unidades del objeto HuevoSorpresa
      */
     public void setUnidades(int unidades) {
         this.unidades = unidades;
     }
 
-    /**
-     * @return the precio
+    /** Método que nos devuelve el precio del objeto HuevoSorpresa
+     * @return devuelve el precio del objeto HuevoSorpresa
      */
     public double getPrecio() {
         return precio;
     }
 
-    /**
-     * @param precio the precio to set
+    /** Método que nos permite modificar el precio del objeto HuevoSorpresa
+     * @param precio introducimos el nuevo precio del objeto HuevoSorpresa
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
-    /**
-     * @return the sorpresa
+    /** Método que nos devuelve la sorpresa que lleva dentro el objeto HuevoSorpresa
+     * @return devuelve la sorpresa que lleva dentro el objeto HuevoSorpresa
      */
     public String getSorpresa() {
         return sorpresa;
     }
 
-    /**
-     * @param sorpresa the sorpresa to set
+    /** Método para modificar la sorpresa del objeto HuevoSorpresa
+     * @param sorpresa escribimos la nueva sorpresa del objeto HuevoSorpresa
      */
     public void setSorpresa(String sorpresa) {
         this.sorpresa = sorpresa;
     }
 
-    /**
-     * @return the precio_max
+    /** Método que nos devuelve el precio máximo del objeto HuevoSorpresa
+     * @return nos devuelve el precio máximo del objeto HuevoSorpresa
      */
     public double getPrecio_max() {
         return precio_max;
     }
 
-    /**
-     * @param precio_max the precio_max to set
+    /** Método para cambiar el precio máximo del objeto HuevoSorpresa
+     * @param precio_max Escribimos el nuevo precio del objeto HuevoSorpresa
      */
     public void setPrecio_max(double precio_max) {
         this.precio_max = precio_max;
